d030813b2e0f937f259641089be8a3b3a22f2ce4
ingridGuimas <ingrisguima@gmail.com>
2025-11-22 14:21:37 -0300

    Delete Projeto_final_programcao_web directory

--- Projeto_final_programcao_web/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    define("HOST", "localhost");
-    define("USER", "root");
-    define("PASS", "");
-    define("BASE", "biblioteca_2122n");
-
-    $conn = new MYSQLi(HOST, USER, PASS, BASE);
